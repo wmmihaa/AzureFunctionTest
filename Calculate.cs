fcb84f73a79adece5dce9c4a5d39a938b08e0efb
Mikael Håkansson <wmmihaa@hotmail.com>
2017-05-11 13:16:34 +0200

    ...

--- Calculate.cs
+++ Calculate.cs
@@ -14,21 +14,8 @@ namespace MyFunction
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
-
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
-
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
-
-            return name == null
-                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            
+            return req.CreateResponse(HttpStatusCode.OK, "Hello Nodemi");
         }
 
     }
